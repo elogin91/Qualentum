0e482719ff0c26e3daf047a784343398c70a0300
elogin91 <elo.programacion@gmail.com>
2024-07-21 11:38:37 +0200

    Adding lab 4

--- Sprint 1 - Lab 4/main.py
+++ Sprint 1 - Lab 4/main.py
@@ -13,24 +13,25 @@ def read_root():
 @app.get("/suma/")
 def suma(a: int, b: int):
     return {"resultado": a + b}
-
+    # http://127.0.0.1:8000/suma/?a=3&b=2
 
 @app.get("/resta/")
 def resta(a: int, b: int):
     return {"resultado": a - b}
-
+    # http://127.0.0.1:8000/resta/?a=3&b=2
 
 @app.get("/multiplicacion/")
 def multiplicacion(a: int, b: int):
     return {"resultado": a * b}
-
+    # http://127.0.0.1:8000/multiplicacion/?a=3&b=2
 
 @app.get("/division/")
 def division(a: int, b: int):
     if b == 0:
         raise HTTPException(status_code=400, detail="No se puede dividir por cero")
     return {"resultado": a / b}
-
+    # http://127.0.0.1:8000/division/?a=3&b=2
+    # http://127.0.0.1:8000/division/?a=3&b=0
 
 if __name__ == "__main__":
     import uvicorn
