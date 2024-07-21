724e719c2e3749a2dd4e8469269bcbe3f1636a5b
elogin91 <elo.programacion@gmail.com>
2024-07-21 12:03:03 +0200

    test_resta

--- Sprint 1 - Lab 5/test_suma.py
+++ Sprint 1 - Lab 5/test_suma.py
@@ -1,4 +1,5 @@
 import unittest
+import sys
 
 from suma import sumar
 class TestSumar(unittest.TestCase):
@@ -6,5 +7,8 @@ class TestSumar(unittest.TestCase):
         self.assertEqual(sumar(3, 2), 5)
         self.assertEqual(sumar(-1, 1), 0)
         self.assertEqual(sumar(-1, -1), -2)
+        self.assertEqual(sumar(sys.maxsize, 1), sys.maxsize + 1)
+        self.assertEqual(sumar(sys.maxsize, sys.maxsize), sys.maxsize * 2)
+        self.assertEqual(sumar(0, sys.maxsize), sys.maxsize)
 if __name__ == '__main__':
     unittest.main()
